c6234035bcb0488b5159445716881d2a0735c8e6
“ZhuXia0” <2628334848@qq.com>
2025-07-04 10:55:26 +0800

    修bug

--- cmd/Coordinator/main.go
+++ cmd/Coordinator/main.go
@@ -13,7 +13,7 @@ func main() {
 	// 注册初始化协调器的接口
 	router.POST("/api/coordinator/init", services.InitHandler)
 
-	port := "8080"
+	port := "8060"
 	fmt.Printf("Coordinator HTTP server running on port %s\n", port)
 	if err := router.Run(":" + port); err != nil {
 		panic(err)
